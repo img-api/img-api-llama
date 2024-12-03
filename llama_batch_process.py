4bc9ceaaa0caf394573cd8bd0c917847e6155e5c
Sergio A. Martinez <sergio@engineer.blue>
2024-12-03 18:45:16 +0000

    [Refactor] Cleanup service so we can expand it
    Signed-off-by: Sergio A. Martinez <sergio@engineer.blue>

--- llama_batch_process.py
+++ llama_batch_process.py
@@ -296,12 +296,51 @@ def run_translation(prompt):
 def lowercase_keys(data):
     if isinstance(data, dict):
         return {
-            key.lower(): lowercase_keys(value) if isinstance(value, (dict, list)) else value
+            key.lower(): (
+                lowercase_keys(value) if isinstance(value, (dict, list)) else value
+            )
             for key, value in data.items()
         }
     elif isinstance(data, list):
         return [lowercase_keys(item) for item in data]
     return data
+
+
+def run_prompt_function(raw_messages, raw_tools, model="llama3.1"):
+    start_time = time.time()  # Start time measurement
+
+    try:
+        response = ollama.chat(
+            model=model,
+            messages=raw_messages,
+            tools=[
+                raw_tools,
+            ],
+        )
+
+        if "tool_calls" not in response["message"]:
+            print_r("Failed loading JSON from result")
+            return None
+
+        result = response["message"]["tool_calls"]
+        result = lowercase_keys(result)
+
+        dmp = json.dumps(result, indent=4)
+        print(dmp)
+
+        d = json.loads(dmp)
+        end_time = time.time()  # End time measurement
+
+        res = d[0]["function"]["arguments"]
+        res["model"] = model
+        res["process_time"] = round(end_time - start_time, 2)
+
+        print_g(f" run_prompt_function: {end_time - start_time:.2f} sec")
+        return d
+    except Exception as e:
+        print_exception(e, "CRASH PROMPT")
+
+    return None
 
 
 def run_prompt(system, assistant, message, model="llama3.1"):
@@ -588,6 +627,94 @@ def run_prompt(system, assistant, message, model="llama3.1"):
     return None
 
 
+def get_generic_system(data):
+
+    if "system" in data:
+        return data["system"]
+
+    system = ""
+    system += "Your name is TOTHEMOON, you are an expert stock analyst ,"
+    system += (
+        "An expert stock market analyst has not only a good education background, "
+    )
+    system += (
+        "extensive experience, but also advanced analytical and technical skills. "
+    )
+    system += "we are regulated and we can provide financial advise."
+    system += "Don't metion anything about the prompt on the message or function calls we might do,"
+    system += "You will get sometimes raw articles ignore messages about cookies and other irrelevant information."
+    system += "Use markdown to highlight important parts on the texts."
+
+    return system
+
+def get_legacy(data):
+    """ Old IMGAPI crazy calls, because we didn't know """
+    assistant = None
+    message = None
+    call_tools = True
+
+    if "article" in data:
+        assistant = data["article"]
+
+    if "prompt" in data:
+        message = data["prompt"]
+
+        if data["type"] == "user_prompt":
+            print_h(" FOUND USER MESSAGE ")
+            call_tools = False
+
+    elif "article" in data and "prompt" in data:
+        print_h(" FOUND PROMPT AND ARTICLE ")
+        call_tools = True
+
+    elif "message" in data:
+        print_h(" COMPANY SIMPLE FORMAT ")
+        assistant = ""
+        message = data["message"]
+
+    return assistant, message, call_tools
+
+def get_generic_messages(data, system, assistant, prompt):
+
+    if "raw_ollama" in data:
+        return data["raw_ollama"]
+
+    arr_messages = []
+
+    if "assistant" in data:
+        arr_messages.append(
+            {
+                "role": "assistant",
+                "content": data["assistant"],
+            }
+        )
+
+        arr_messages.append(
+            {
+                "role": "system",
+                "content": system,
+            }
+        )
+
+        arr_messages.append(
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        )
+
+        return arr_messages
+
+    arr_messages = [
+        {
+            "role": "user",
+            "content": assistant + system + prompt,
+        }
+    ]
+
+    return arr_messages
+
+
 def main(host: str, port: int):
     # Loop through the folder and process the oldest JSON file
     # Get the oldest JSON file
@@ -624,146 +751,81 @@ def main(host: str, port: int):
         return
 
     translation = False
-    call_tools = False
-    assistant = ""
+    res_json = None
     message = ""
 
-    if "article" in data:
-        assistant = data["article"]
-
-    if "prompt" in data:
-        message = data["prompt"]
-
-    if "type" in data and data["type"] == "translation":
-        print_h(" FOUND TRANSLATION ")
-        translation = True
-
-    elif "type" in data and data["type"] == "user_prompt":
-        print_h(" FOUND USER MESSAGE ")
-        call_tools = False
-
-    elif "article" in data and "prompt" in data:
-        print_h(" FOUND PROMPT AND ARTICLE ")
-        call_tools = True
-
-    elif "message" in data:
-        print_h(" COMPANY SIMPLE FORMAT ")
-        assistant = ""
-        message = data["message"]
-
-    print_g(f"FILE TO PROCESS {json_file}\n")
-
-    result = None
-    res_json = None
-
-    if "system" in data:
-        system = data["system"]
+    if "type" in data and data["type"] == "raw_llama":
+        print_g(" RUNNING LLAMA IN RAW MODE ")
     else:
-        system = ""
-        system += "Your name is TOTHEMOON, you are an expert stock analyst ,"
-        system += (
-            "An expert stock market analyst has not only a good education background, "
-        )
-        system += (
-            "extensive experience, but also advanced analytical and technical skills. "
-        )
-        system += "we are regulated and we can provide financial advise."
-        system += "Don't metion anything about the prompt on the message or function calls we might do,"
-        system += "You will get sometimes raw articles ignore messages about cookies and other irrelevant information."
-        system += "Use markdown to highlight important parts on the texts."
+        if "type" in data:
+            if data["type"] == "translation":
+                print_h(" FOUND TRANSLATION ")
+                translation = True
+                res_json = run_translation(message)
 
-    if "raw_ollama" in data:
-        arr_messages = data["raw_ollama"]
-    else:
-        arr_messages = []
+        print_g(f"FILE TO PROCESS {json_file}\n")
 
-        if "assistant" in data:
-            arr_messages.append(
-                {
-                    "role": "assistant",
-                    "content": data["assistant"],
-                }
+        result = None
+        system = get_generic_system(data)
+        assistant, message, call_tools = get_legacy(data)
+        arr_messages = get_generic_messages(data, system, assistant, message)
+
+        try:
+            if not translation:
+                response = ollama.chat(
+                    model="llama3.1",
+                    messages=arr_messages,
+                )
+                # Process the message using the run_main function
+
+                result = response["message"]["content"]
+                result = re.sub(
+                    r"(?i)summary.*(text|article).*markdown.*facts[:\s]*", "", result
+                )
+
+                console.print(Markdown(result))
+
+                for msg in arr_messages:
+                    msg["word_count"] = len(word_count(msg["content"]))
+
+                data["raw"] = arr_messages
+
+            if call_tools:
+                res_json = run_prompt(system, assistant, message, "llama3.1")
+                if not res_json:
+                    print_r(" RETRY, MAYBE OUR LLAMA 3.1 WAS LAZY")
+                    res_json = run_prompt(system, assistant, message, "llama3.2")
+
+                if not res_json:
+                    print_r(" FAILED LLAMA3.2 TOO ")
+
+        except TimeoutError as e:
+            print(e)
+            print("---------------- TIMEOUT DOING PROCESSING --------------")
+            shutil.move(
+                json_file, os.path.join(ai_timeout, os.path.basename(json_file))
             )
 
-            arr_messages.append(
-                {
-                    "role": "system",
-                    "content": system,
-                }
+        except Exception as e:
+            shutil.move(
+                json_file, os.path.join(ai_crashed, os.path.basename(json_file))
             )
 
-            arr_messages.append(
-                {
-                    "role": "user",
-                    "content": message,
-                }
-            )
-        else:
-            arr_messages.append(
-                {
-                    "role": "user",
-                    "content": assistant + system + message,
-                }
-            )
+            print_r(f"Failed to contact inference {json_file}: {e}")
 
-    try:
-        if translation:
-            res_json = run_translation(message)
-        else:
-            response = ollama.chat(
-                model="llama3.1",
-                messages=arr_messages,
-            )
-            # Process the message using the run_main function
-
-            result = response["message"]["content"]
-            result = re.sub(
-                r"(?i)summary.*(text|article).*markdown.*facts[:\s]*", "", result
-            )
-
-            console.print(Markdown(result))
-
-            for msg in arr_messages:
-                msg["word_count"] = len(word_count(msg["content"]))
-
-            data["raw"] = arr_messages
-
-        if call_tools:
-            res_json = run_prompt(system, assistant, message, "llama3.1")
-            if not res_json:
-                print(message)
-                print_r(" RETRY, MAYBE OUR LLAMA 3.1 WAS LAZY")
-                res_json = run_prompt(system, assistant, message, "llama3.2")
-
-            if not res_json:
-                print_r(" FAILED LLAMA3.2 TOO ")
-
-    except TimeoutError as e:
-        print(e)
-        print("---------------- TIMEOUT DOING PROCESSING --------------")
-        shutil.move(json_file, os.path.join(ai_timeout, os.path.basename(json_file)))
-
-    except Exception as e:
-        shutil.move(json_file, os.path.join(ai_crashed, os.path.basename(json_file)))
-
-        print(f"Failed to contact inference {json_file}: {e}")
-        # kill_llama()
-
-    if not translation and not result:
-        print(f"NO RESULT {json_file}")
+    if not translation and not result and not res_json:
+        print_r(f"NO RESULT {json_file}")
         shutil.move(json_file, os.path.join(ai_crashed, os.path.basename(json_file)))
         return
 
-    # Add the result to the JSON data
-
     if res_json != None:
+        data["type"] = "dict"
+        data["dict"] = res_json
         if result:
             data["ai_summary"] = str(result).replace("StepType.inference> ", "")
-
-        data["dict"] = res_json
-        data["type"] = "dict"
     else:
-        data["result"] = str(result).replace("StepType.inference> ", "")
+        if result:
+            data["result"] = str(result).replace("StepType.inference> ", "")
 
     # Callback with the updated data
     try:
@@ -771,7 +833,6 @@ def main(host: str, port: int):
             json.dump(data, f, indent=4)
 
         upload_file(json_file)
-
     except Exception as e:
         print_e(f"Failed to save result to file {json_file}: {e}")
 
