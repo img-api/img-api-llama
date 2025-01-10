e5b35beb70859c2c41122c45ac187b7d429a23ad
Sergio A. Martinez <sergio@engineer.blue>
2025-01-10 15:43:02 +0000

    Move moethod to common folder

--- llama_batch_process.py
+++ llama_batch_process.py
@@ -20,6 +20,9 @@ from collections import Counter
 from rich.console import Console
 from rich.markdown import Markdown
 
+from .files import get_oldest_file, load_json_file
+
+
 console = Console()
 warnings.filterwarnings("ignore", category=urllib3.exceptions.InsecureRequestWarning)
 http = urllib3.PoolManager()
@@ -43,10 +46,6 @@ def word_count(text):
 
 def print_w(text):
     print(Fore.LIGHTWHITE_EX + text)
-
-
-def print_b(text):
-    print(Fore.LIGHTBLUE_EX + text)
 
 
 def print_g(text, in_place=False):
@@ -238,60 +237,9 @@ def get_oldest_file_by_priority(folder):
     return arr[0]
 
 
-def get_oldest_file(folder):
-    """Get the oldest file in the folder."""
-    files = [os.path.join(folder, f) for f in os.listdir(folder) if f.endswith(".json")]
-    if not files:
-        return None
-    oldest_file = min(files, key=os.path.getctime)
-    return oldest_file
-
-
 def update_file_timestamp(filepath):
     """Touch the file to update its timestamp, making it the 'youngest'."""
     os.utime(filepath, None)
-
-
-def callback_url(url, data):
-    """Send a callback to the URL with the processed data."""
-    try:
-        response = requests.post(url, json=data, verify=False)
-        response.raise_for_status()
-        print_g(f" Callback {url} {response.status_code}")
-
-        return True
-    except requests.exceptions.RequestException as e:
-        print_e(f" Failed to callback {url}: {e}")
-
-    return False
-
-
-def upload_file(json_file):
-    try:
-        with open(json_file, "r") as f:
-            data = json.load(f)
-
-            result_ok = callback_url(data["callback_url"], data)
-
-            if "dev" in data:
-                if result_ok:
-                    api_file_move(json_file, development_folder)
-                else:
-                    api_file_move(json_file + ".FAILED", development_folder)
-
-            else:
-                if result_ok:
-                    api_file_move(json_file, processed_folder)
-                else:
-                    api_file_move(json_file, failed_folder)
-
-            print("\n")
-
-    except Exception as e:
-        print(f"Failed to save result to file {json_file}: {e}")
-
-        os.remove(json_file)  # Delete the file if it can't be saved
-        print(f"Deleted file due to save error: {json_file}")
 
 
 def kill_llama():
@@ -988,13 +936,6 @@ def get_generic_messages(data, system, assistant, prompt):
     return arr_messages
 
 
-def api_file_move(json_file, new_folder):
-    print_b(" " + os.path.basename(json_file) + " >> " + new_folder)
-    ret = os.path.join(new_folder, os.path.basename(json_file))
-    shutil.move(json_file, ret)
-    return ret
-
-
 def api_update_stats(total_time):
     stats_file = ".stats.json"
     try:
@@ -1046,20 +987,7 @@ def main(host: str, port: int):
         return
 
     # Load the JSON data
-    try:
-        with open(json_file, "r") as f:
-            data = json.load(f)
-    except json.JSONDecodeError as e:
-        print_e(f"Invalid JSON format in file: {json_file}. Error: {e}")
-        os.remove(json_file)  # Delete the file if it's invalid
-        print_r(f"Deleted invalid file: {json_file}")
-        return
-
-    # Check the expected format
-    if "id" not in data or "callback_url" not in data:
-        print_e(f"Invalid format in file: {json_file}")
-        os.remove(json_file)
-        return
+    data = load_json_file(json_file)
 
     print_w(" " + data["id"])
 
